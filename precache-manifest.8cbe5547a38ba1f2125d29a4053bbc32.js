30fbf619332985805d9961621d08783dd7ffd85b
FerretsNook <yarnferret@gmail.com>
2020-05-25 20:33:06 +0200

    Updates

---
+++ precache-manifest.8cbe5547a38ba1f2125d29a4053bbc32.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "085287a6aded63e49f5d789804f26098",
+    "url": "/PokApi/index.html"
+  },
+  {
+    "revision": "5fa09160c3af036d92a6",
+    "url": "/PokApi/static/css/main.89fc6a7a.chunk.css"
+  },
+  {
+    "revision": "8bdce2c1d386fe088058",
+    "url": "/PokApi/static/js/2.91a1577f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PokApi/static/js/2.91a1577f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa09160c3af036d92a6",
+    "url": "/PokApi/static/js/main.6de1e544.chunk.js"
+  },
+  {
+    "revision": "49d401500ac4f9ce38b0",
+    "url": "/PokApi/static/js/runtime-main.27ddd1df.js"
+  },
+  {
+    "revision": "188e0e531c8c950a48b71a9e490b4481",
+    "url": "/PokApi/static/media/Baloo2-Medium.188e0e53.ttf"
+  },
+  {
+    "revision": "a95cb88be1095a0cf15b8bd8b8566149",
+    "url": "/PokApi/static/media/Baloo2-Regular.a95cb88b.ttf"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/PokApi/static/media/Pokemon Solid.36d26dbc.ttf"
+  },
+  {
+    "revision": "71468b877880e39507242e32d0ba65e2",
+    "url": "/PokApi/static/media/pokeball.71468b87.png"
+  }
+]);
